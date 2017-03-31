a1e7444193a9cd77a49b66f3f4a62344d18225d0
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-31 15:22:32 -0400

    working version

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var app = express();
+var multer  = require('multer');
+var upload = multer({dest: 'uploads/'});
 app.use(express.static(__dirname + "/view"));
 app.get("/", function(req, res) {
 	res.sendFile("index.html");
+});
+app.post("/upload", upload.single("file"), function(req, res, next) {
+	if(req.file) {
+		res.json({size: req.file.size});
+	} else {
+		res.send({err: "File not found."});
+	}
 });
 app.listen(process.env.PORT || 3000);
